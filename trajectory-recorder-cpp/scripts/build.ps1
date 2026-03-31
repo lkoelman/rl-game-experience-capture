5b46ffb4942521a92ee70fc8fc7ad45225ba809a
Lucas Koelman <lucas.koelman@gmail.com>
2026-03-31 17:39:28 +0100

    Fix build script

--- trajectory-recorder-cpp/scripts/build.ps1
+++ trajectory-recorder-cpp/scripts/build.ps1
@@ -7,12 +7,10 @@ $ErrorActionPreference = "Stop"
 
 conan install . --output-folder=$BuildDir --build=missing
 
-$buildScript = @"
-call "$BuildDir\conanbuild.bat" &&
-meson setup "$BuildDir" --buildtype $BuildType --native-file "$BuildDir\conan_meson_native.ini" --reconfigure &&
-meson compile -C "$BuildDir" &&
-meson test -C "$BuildDir" --print-errorlogs
-"@
+$buildScript = "call ""$BuildDir\conanbuild.bat"" && " +
+    "meson setup ""$BuildDir"" --buildtype $BuildType --native-file ""$BuildDir\conan_meson_native.ini"" --reconfigure && " +
+    "meson compile -C ""$BuildDir"" && " +
+    "meson test -C ""$BuildDir"" --print-errorlogs"
 
 cmd /c $buildScript
 if ($LASTEXITCODE -ne 0) {
